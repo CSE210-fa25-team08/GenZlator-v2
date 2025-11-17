6ed00e9e849ce6678d6cc63ba06025d8312956e8
Chih-Yun Lin <noralin249@gmail.com>
2025-11-17 02:24:24 -0800

    feedback after shortcut

--- src/frontend/slack-bot/src/routes/shortcuts.js
+++ src/frontend/slack-bot/src/routes/shortcuts.js
@@ -1,4 +1,5 @@
 // src/routes/shortcuts.js
+const { buildFeedbackBlocks } = require("../utils/feedback");
 
 module.exports = function registerShortcuts(app) {
     // the shortcut ID is "translate_shortcut", and need to align with the one set in Slack App configuration
@@ -7,9 +8,16 @@ module.exports = function registerShortcuts(app) {
         await ack();
         const originalText = body.message?.text || "(no text)";
 
+        const feedbackBlocks = buildFeedbackBlocks(originalText);
+
         await client.chat.postMessage({
             channel: body.channel.id,
             text: `Translated: "${originalText}" → DUMMY_OUTPUT`
+        });
+
+        await client.chat.postMessage({
+            channel: body.channel.id,
+            blocks: feedbackBlocks,
         });
     });
 };
